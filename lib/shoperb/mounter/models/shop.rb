3c26c495e34e14dc3d74261088bd957d9c1159d8
Rainer Sai <rainersai@gmail.com>
2014-09-07 15:56:13 +0300

    add proper error message for missing currency

--- lib/shoperb/mounter/models/shop.rb
+++ lib/shoperb/mounter/models/shop.rb
@@ -9,6 +9,11 @@ module Shoperb
           Language.all.map(&:code)
         end
 
+        def currency_with_check
+          currency_without_check || raise(Error.new("Shops currency has not been set"))
+        end
+        alias_method_chain :currency, :check
+
       end
     end
   end
