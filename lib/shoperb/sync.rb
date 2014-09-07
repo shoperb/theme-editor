a5ce0281ddf78c975c6a977df98e71a4b1f3f854
Rainer Sai <rainersai@gmail.com>
2014-09-07 15:58:03 +0300

    sync images to their own file, download them

--- lib/shoperb/sync.rb
+++ lib/shoperb/sync.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 module Shoperb
   module Sync
     extend self
@@ -11,8 +13,12 @@ module Shoperb
         self.file = Utils.base + "data" + "#{name}.yml"
       end
 
+      def current_content
+        File.exists?(file) ? File.read(file).force_encoding("utf-8") : ""
+      end
+
       def replace name, hash
-        content = YAML::load(File.read(file).force_encoding("utf-8"))
+        content = YAML::load(current_content) || {}
         (content[collection_name] || {}).delete_if { |i| i["name"] == name }
         yield(hash) if block_given?
         (content[collection_name] ||= []) << hash
@@ -22,15 +28,30 @@ module Shoperb
       end
     end
 
+    def image hash, token
+      Utils.mkdir(dir = Utils.base + "assets" + "images")
+      hash["sizes"] = hash["sizes"].map do |size, url|
+        next unless url
+        filename = dir + "#{hash["name"]}_#{size}#{Pathname.new(url).extname.split("?")[0]}"
+        Utils.write_file(filename) { open(url).read }
+        [size, filename.relative_path_from(dir).to_s]
+      end.compact.to_h
+      Replacer.new("images").replace(hash["name"], hash)
+    end
 
-    %w{products categories images collections vendors}.each do |plural|
+    %w{products categories collections vendors}.each do |plural|
       define_method plural do |token|
         token.get(plural).parsed.each { |hash| send(plural.singularize, hash, token) }
       end
 
       define_method plural.singularize do |hash, token|
-        Logger.notify "Syncing #{plural.singularize} named #{hash["name"]}" do
-          Replacer.new(plural).replace(hash["name"], hash)
+        Replacer.new(plural).replace(hash["name"], hash) do |item|
+          item.delete_if { |key,value|
+            value.map { |sub_item|
+              sub_item["#{plural.singularize}_name"] = hash["name"]
+              Sync.send(key.singularize, sub_item, token)
+            } if value.is_a?(Array) && value.none?{|v|!v.is_a?(Hash)}
+          }
         end
       end
     end
