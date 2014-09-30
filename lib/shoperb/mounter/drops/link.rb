74dd6ea262b6b5f25691b84787e44dfe03ad0b19
Rainer Sai <rainer.sai@perfectline.co>
2014-09-30 18:07:50 +0300

    link url

--- lib/shoperb/mounter/drops/link.rb
+++ lib/shoperb/mounter/drops/link.rb
@@ -3,6 +3,18 @@ module Shoperb
     module Drop
       class Link < Delegate
 
+        def url
+          case record.style
+            when "PRODUCTS"
+              "/products"
+            when "Page"
+              record.entity.to_liquid.url
+            when "Collection"
+              "javascript:void(0)"
+            else
+              record.value
+          end
+        end
       end
     end
   end
--- lib/shoperb/mounter/drops/variant.rb
+++ lib/shoperb/mounter/drops/variant.rb
@@ -4,7 +4,7 @@ module Shoperb
       class Variant < Delegate
 
         def options
-          @record.variant_attributes.map(&:value)
+          record.variant_attributes.map(&:value)
         end
 
         def image
@@ -17,6 +17,34 @@ module Shoperb
 
         def attributes
           __to_drop__ Drop::Collection, :variant_attributes
+        end
+
+        def discount_price
+          record.price_discount
+        end
+
+        def discount?
+          record.discount_active?
+        end
+
+        def discount_period?
+          record.has_discount_range?
+        end
+
+        def discount_start
+          record.formatted_discount_start
+        end
+
+        def discount_end
+          record.formatted_discount_end
+        end
+
+        def current_price
+          record.active_price
+        end
+
+        def stock
+          record.track_inventory? ? record.stock : nil
         end
 
         def json
