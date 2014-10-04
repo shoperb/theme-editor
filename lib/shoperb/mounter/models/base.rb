efb2081f598e8977f274ab64ef81ac097990f0e5
Rainer Sai <rainersai@gmail.com>
2014-10-05 02:15:34 +0300

    automatic values for relations

--- lib/shoperb/mounter/models/base.rb
+++ lib/shoperb/mounter/models/base.rb
@@ -7,6 +7,28 @@ module Shoperb
         include ActiveHash::Associations
 
         class << self
+
+          def belongs_to_with_auto_key(association_id, options = {})
+            klass = parent.const_get(association_id.to_s.classify)
+            options.reverse_merge!(
+              class_name: klass.to_s,
+              foreign_key: "#{association_id}_#{klass.primary_key}",
+              primary_key: klass.primary_key
+            )
+            belongs_to_without_auto_key(association_id, options)
+          end
+          alias_method_chain :belongs_to, :auto_key
+
+          def has_many_with_auto_key(association_id, options = {})
+            klass = parent.const_get(association_id.to_s.classify)
+            options.reverse_merge!(
+              class_name: klass.to_s,
+              foreign_key: "#{to_s.demodulize.underscore}_#{primary_key}",
+            )
+            has_many_without_auto_key(association_id, options)
+          end
+          alias_method_chain :has_many, :auto_key
+
           def load_path(path)
             File.exists?(path) ? YAML.load_file(path) : []
           end
