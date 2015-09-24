7706cc6f79ae975872114027822f51089088aa0a
Rainer Sai <rainer.sai@perfectline.co>
2015-09-24 16:57:32 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
+if [[ -f /etc/lsb-release ]]; then
+    os=$DISTRIB_ID
+    version=$DISTRIB_RELEASE
+else
+    os=$(uname -s)
+    version=$(sw_vers -productVersion)
+fi
 
-case $(uname) in
-    Ubuntu)
-        which git || sudo apt-get install git
-        ;;
-esac
+install_git () {
+    if [[ os -eq "Ubuntu" ]]; then
+        sudo apt-get -y install git </dev/null
+    fi
+}
 
-function install_rvm {
+install_rvm () {
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable
     source ~/.rvm/scripts/rvm
     rvm install 2.2.1
     /bin/bash --login
 }
+
+install_bundler () {
+    if [[ os -eq "Ubuntu" ]]; then
+        if [[ version -eq "14.04" ]]; then
+            sudo apt-get -y install bundler </dev/null
+        else
+            gem install bundler
+        fi
+    else
+        gem install bundler
+    fi
+}
+
+which git || install_git
 which rvm || install_rvm
+
 rvm use 2.2.1
 echo "2.2.1" > .ruby-version
 echo "source \"https://rubygems.org\"" > Gemfile
 echo "gem \"shoperb-theme-editor\", github: \"shoperb/theme-editor\"" >> Gemfile
-gem install bundler
+
+install_bundler
+
 bundle
