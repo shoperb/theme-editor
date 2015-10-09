ba1f95b3eeed7e94768ab1859474bdf228b13fe7
Rainer Sai <rainer.sai@perfectline.co>
2015-10-09 13:28:13 +0300

    link parents

--- lib/shoperb_theme_editor/mounter/models/link.rb
+++ lib/shoperb_theme_editor/mounter/models/link.rb
@@ -3,7 +3,7 @@ module Shoperb module Theme module Editor
     module Model
       class Link < Base
 
-        fields :id, :entity_id, :entity_type, :style, :name, :value, :handle, :position, :translations
+        fields :id, :entity_id, :parent_id, :entity_type, :style, :name, :value, :handle, :position, :translations
 
         translates :name, :description
 
@@ -11,6 +11,14 @@ module Shoperb module Theme module Editor
           :handle
         end
         belongs_to :menu
+
+        def parent
+          Link.all.detect { |parent| parent.attributes[:id] == self.parent_id }
+        end
+
+        def children
+          Link.all.select { |child| child.parent_id == attributes[:id] }
+        end
 
         def entity
           Model.const_get(entity_type).all.detect { |object| object.attributes[:id] == entity_id } if entity_type && entity_id
--- shoperb_theme_liquid/lib/drops/menu.rb
+++ shoperb_theme_liquid/lib/drops/menu.rb
@@ -14,7 +14,15 @@ module Shoperb module Theme module Liquid module Drop
     end
 
     def links
-      Collection.new(record.links)
+      Collection.new(record.links).tap do |drop|
+        drop.context = @context
+      end
+    end
+
+    def root_links
+      Collection.new(record.links.detect { |link| link.parent_id == nil }).tap do |drop|
+        drop.context = @context
+      end
     end
 
   end
