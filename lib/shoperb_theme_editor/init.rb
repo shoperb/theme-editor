b724c031b474a673165a86c343a95891b0d23c53
Rainer Sai <rainer.sai@perfectline.co>
2015-10-09 18:40:43 +0300

    load all from init dir as possible templates

--- lib/shoperb_theme_editor/init.rb
+++ lib/shoperb_theme_editor/init.rb
@@ -1,14 +1,16 @@
 module Shoperb module Theme module Editor
   class Init
 
-    AVAILABLE_TEMPLATES = ["blank"]
-
     Editor.autoload_all self, "init"
+
+    def self.available_templates
+      Dir.glob("#{File.expand_path("../init/*", __FILE__)}").map(&Pathname.method(:new)).map(&:basename).map(&:to_s)
+    end
 
     def initialize template, handle
       raise Error.new(
-        "No such template, possible options are #{AVAILABLE_TEMPLATES.map(&:inspect).to_sentence}"
-      ) unless AVAILABLE_TEMPLATES.include?(template)
+        "No such template, possible options are #{self.class.available_templates.map(&:inspect).to_sentence}"
+      ) unless self.class.available_templates.include?(template)
       Editor["handle"] = handle
       Logger.notify "Copying #{template.inspect} template" do
         FileUtils.cp_r("#{File.expand_path("../init/#{template}", __FILE__)}/.", Utils.base + ".")
