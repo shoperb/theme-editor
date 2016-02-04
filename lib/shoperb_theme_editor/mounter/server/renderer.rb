93de71298abeb0c1f83ca994822f21739a55a48d
Rainer Sai <rainer.sai@perfectline.co>
2016-02-04 11:02:04 +0200

    add shop address, remove indifference, seems to break liquid context

--- lib/shoperb_theme_editor/mounter/server/renderer.rb
+++ lib/shoperb_theme_editor/mounter/server/renderer.rb
@@ -45,40 +45,7 @@ module Shoperb module Theme module Editor
           end
 
           def respond_email(templates, locals={}, registers={}, &block)
-            respond(templates, locals, {layout:nil}.with_indifferent_access.reverse_merge(registers), settings.emails_directory, &block)
-          end
-
-          def respond_any(templates, locals={}, registers={}, &block)
-            registers.reverse_merge!(
-              replace_locale: ->(locale) {
-                request.fullpath.gsub(/\A(?=#{shop.possible_languages.map{|s|"/#{s}"}.join("|")}|)\/(.*)/, "/#{locale}/\\1")
-              },
-              empty_collection: -> {
-                Drop::Collection.new(Kaminari::PaginatableArray.new)
-              },
-              controller: self,
-              asset_url: ->(url, *args) { "/system/assets/#{url}" },
-              translate: Translations.method(:translate),
-              locale: Translations.locale,
-              shop: shop,
-              category: request.env[:current_category],
-              request_forgery_protection_token: -> { %(<input type="hidden" name="" value="">) },
-              models: Model
-            )
-            locals = locals.reverse_merge(default_locals)
-            registers = registers.reverse_merge({layout: "layout"})
-            templates = [templates].flatten
-            format = Sinatra::RespondWith::Format.new(self)
-            if (template = template_name(templates))
-              locals.merge!(template_name: template.to_s)
-              file = template_path(template)
-              result = process_file file, locals, registers
-              unless (layout_name = registers[:layout].to_s).blank?
-                result = process_file(template_path(layout_name, settings.layouts_directory), locals.merge(content_for_layout: result), registers)
-              end
-              halt result
-            end
-            format.finish(&block)
+            respond(templates, locals, {layout:nil}.reverse_merge(registers), settings.emails_directory, &block)
           end
 
           def process_file file, locals, registers
