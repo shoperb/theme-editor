317689b96685836e29564dfb6b04790c2f749ba9
Rainer Sai <rainer.sai@perfectline.co>
2016-02-04 11:45:02 +0200

    order/order_item

--- lib/shoperb_theme_editor/mounter/models/order.rb
+++ lib/shoperb_theme_editor/mounter/models/order.rb
@@ -3,9 +3,15 @@ module Shoperb module Theme module Editor
     module Model
       class Order < Base
 
+        fields :id, :number, :token, :email, :total, :subtotal, :total_shipping, :total_taxes, :require_shipping, :require_taxation, :created_at, :state, :notes
+
         has_one :shipping_method
 
-        # todo: TODOREF2
+        belongs_to :ship_address
+
+        belongs_to :bill_address
+
+        belongs_to :customer
       end
     end
   end
