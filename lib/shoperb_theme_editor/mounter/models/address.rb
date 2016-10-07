d669637de23b9fc85ba9a8addfbfe7f5276786d1
Rainer Sai <rainer.sai@perfectline.co>
2016-10-07 12:36:09 +0300

    customer drop update

--- lib/shoperb_theme_editor/mounter/models/address.rb
+++ lib/shoperb_theme_editor/mounter/models/address.rb
@@ -6,13 +6,6 @@ module Shoperb module Theme module Editor
         fields :id, :owner_id, :owner_type, :country_id, :state_id, :first_name, :last_name, :phone, :company, :county, :city, :zip, :address1, :address2, :checksum
         fields :email
 
-        # todo: TODOREF2
-        # nothing to use as primary_key besides id right now
-        # def self.primary_key
-        #   :name
-        # end
-        # todo: TODOREF2 end
-
         belongs_to :country
         belongs_to :state
 
--- lib/shoperb_theme_editor/mounter/models/customer.rb
+++ lib/shoperb_theme_editor/mounter/models/customer.rb
@@ -2,7 +2,29 @@ module Shoperb module Theme module Editor
   module Mounter
     module Model
       class Customer < Base
-        # todo: TODOREF2
+        fields :id, :last_bill_address_id, :last_ship_address_id, :first_name, :last_name, :email, :newsletter, :active, :discount_pct, :recommended_products_ids
+
+        has_many :orders
+
+        def last_bill_address
+          Address.all.detect { |address| attributes[:last_bill_address_id] == address.attributes[:id] }
+        end
+
+        def last_ship_address
+          Address.all.detect { |address| attributes[:last_ship_address_id] == address.attributes[:id] }
+        end
+
+        def addresses
+          check = { owner_type: "Address", owner_id: attributes[:id] }.with_indifferent_access
+          Address.all.select { |address|
+            address.attributes.slice(:owner_type, :owner_id).with_indifferent_access == check
+          }
+        end
+
+        def recommended_products
+          possible = attributes.fetch(:recommended_products_ids, []).map(&:to_s)
+          Product.all.select { |product| possible.include?(product.attributes[:id].to_s) }
+        end
       end
     end
   end
