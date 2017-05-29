410b7f36d8199dbdb1efcafffbfe38bff71369e2
Myroslava Stavnycha <myroslava@perfectline.co>
2017-05-29 14:18:20 +0300

    fix binding association variant#product (dunno what else it might affect)

--- lib/shoperb_theme_editor/mounter/models/product.rb
+++ lib/shoperb_theme_editor/mounter/models/product.rb
@@ -9,7 +9,7 @@ module Shoperb module Theme module Editor
         translates :name, :description
 
         def self.primary_key
-          :handle
+          :id
         end
 
         def collections
