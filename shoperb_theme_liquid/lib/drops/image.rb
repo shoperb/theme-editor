1f1843d3656d704a5be80c37cf6f3570f445be5a
Myroslava Stavnycha <myroslava@perfectline.co>
2017-09-15 11:56:23 +0300

    aspect_ratio to image drop

--- shoperb_theme_liquid/lib/drops/image.rb
+++ shoperb_theme_liquid/lib/drops/image.rb
@@ -13,6 +13,10 @@ module Shoperb module Theme module Liquid module Drop
       record.url
     end
 
+    def aspect_ratio
+      height / width
+    end
+
     def before_method(method, *args)
       if image = record.image_sizes.detect { |s| s.name == method.to_s }
         Image.new(image).tap do |drop|
