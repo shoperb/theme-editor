0350704858677aee01a529b4a0509dd2072974c1
Alexander Litvinchuk <alexander@perfectline.co>
2018-11-15 15:28:19 +0200

    return permalink for language

--- lib/shoperb_theme_editor/mounter/models/language.rb
+++ lib/shoperb_theme_editor/mounter/models/language.rb
@@ -5,6 +5,10 @@ module Shoperb module Theme module Editor
 
         fields :id, :code, :name, :native, :active
 
+        def self.primary_key
+          :code
+        end
+
       end
     end
   end
