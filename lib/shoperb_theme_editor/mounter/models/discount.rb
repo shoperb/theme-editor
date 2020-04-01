47b5fcd063b6dc3d7950ffe5f46e03f1a84cf810
Vladimir Krylov <vladimir.krylov@perfectline.co>
2020-04-01 11:13:41 +0300

    discounts pull generates discount variants

--- lib/shoperb_theme_editor/mounter/models/discount.rb
+++ lib/shoperb_theme_editor/mounter/models/discount.rb
@@ -15,6 +15,24 @@ module Shoperb module Theme module Editor
         def variants
           discount_variants.map(&:variant)
         end
+        class << self
+          def assign records
+            self.delete_all
+            self.data = records.map(&method(:filtered_attributes))
+
+            count = 1
+            discs_variants=records.each_with_object([]){|r,a|
+              next if r["target_type"]!="variants"
+              r["target_ids"].each do |variant_id|
+                a << {"id"=> count, "discount_id"=> r["id"], "variant_id"=> variant_id}
+                count+=1
+              end
+            }
+            Mounter::Model::DiscountVariant.assign discs_variants
+
+            records
+          end
+        end
       end
     end
   end
