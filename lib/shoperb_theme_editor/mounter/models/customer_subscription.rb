582f232c1e30cdc7236e05c77e420bfd9e670381
Vladimir Krylov <vladimir.krylov@perfectline.co>
2020-12-21 15:07:24 +0200

    fetch customer subscription drop

--- lib/shoperb_theme_editor/mounter/models/customer_subscription.rb
+++ lib/shoperb_theme_editor/mounter/models/customer_subscription.rb
@@ -4,25 +4,26 @@ module Shoperb module Theme module Editor
     module Model
       class CustomerSubscription < Base
 
-        fields :id, :plan_id, :customer_id, :auto_collection, :state, :qty, 
-          :custom_field_values, :starts_at, :ends_at, :trial_starts_at, :trial_ends_at
+        fields :id, :plan_id, :customer_id, :auto_collection, :state, :qty,
+          :custom_field_values, :starts_at, :ends_at, :trial_starts_at, :trial_ends_at,
+          :gift_card_code
 
         def self.primary_key
           :id
         end
-        
+
         belongs_to :plan, class_name: CustomerSubscriptionPlan.to_s, foreign_key: :plan_id
         belongs_to :customer
-        
-        
+
+
         def active?
           state == "active"
         end
-        
+
         def self.active
           all
         end
-        
+
         def to_liquid context=nil
           if klass = (ShoperbLiquid.const_get("SubscriptionDrop"))
             klass.new(self).tap do |drop|
@@ -30,7 +31,7 @@ module Shoperb module Theme module Editor
             end
           end
         end
-        
+
       end
     end
   end
