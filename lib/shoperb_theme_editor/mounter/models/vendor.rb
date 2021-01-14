4f38a05740671cdb1952b9329d4dd3c4bf837789
Vladimir Krylov <vladimir.krylov@perfectline.co>
2021-01-14 14:06:35 +0200

    permalink

--- lib/shoperb_theme_editor/mounter/models/vendor.rb
+++ lib/shoperb_theme_editor/mounter/models/vendor.rb
@@ -10,7 +10,7 @@ module Shoperb module Theme module Editor
         translates :name, :description, :display_name
 
         def self.primary_key
-          :id
+          :permalink
         end
 
         has_many :products
