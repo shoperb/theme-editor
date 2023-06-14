2b0156f81dfcdedbd20fc3a1bc9dbbe1de265e79
Vladimir Krylov <vladimir.krylov@perfectline.co>
2023-06-14 12:47:35 +0300

    restore raven

--- lib/shoperb_theme_editor/error.rb
+++ lib/shoperb_theme_editor/error.rb
@@ -1,28 +1,28 @@
 require "raven"
 
-# Raven.configure do |config|
-#   config.dsn = 'https://71ae0199b499419da0ffe2a9695871ca:bc1386ebe0d4486aac1e1b41c3011f41@sentry.io/1273398'
-#   config.excluded_exceptions += [
-#     "Liquid::FileSystemError",
-#     "Liquid::ArgumentError",
-#     "Liquid::SyntaxError",
-#     "Liquid::UndefinedFilter",
-#     "Liquid::UndefinedDropMethod",
-#     "Liquid::UndefinedVariable",
-#     "Shoperb::Theme::Editor::Error"
-#   ]
-# end
+Raven.configure do |config|
+  config.dsn = 'https://71ae0199b499419da0ffe2a9695871ca:bc1386ebe0d4486aac1e1b41c3011f41@sentry.io/1273398'
+  config.excluded_exceptions += [
+    "Liquid::FileSystemError",
+    "Liquid::ArgumentError",
+    "Liquid::SyntaxError",
+    "Liquid::UndefinedFilter",
+    "Liquid::UndefinedDropMethod",
+    "Liquid::UndefinedVariable",
+    "Shoperb::Theme::Editor::Error"
+  ]
+end
 
 module Shoperb module Theme module Editor
   class Error < Exception
     def self.report exception
       log(exception)
-      # Raven.capture_exception(exception)
+      Raven.capture_exception(exception)
     end
 
     def self.report_rack exception, env
       log(exception)
-      # Raven::Rack.capture_exception(exception, env)
+      Raven::Rack.capture_exception(exception, env)
     end
 
     def self.log exception
