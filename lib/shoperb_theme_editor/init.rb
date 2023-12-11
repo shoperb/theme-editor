69e81fe5182ad9f1326312fcab82dbde9e6a5af9
Vladimir Krylov <vladimir.krylov@perfectline.co>
2023-12-11 16:24:02 +0200

    nested init

--- lib/shoperb_theme_editor/init.rb
+++ lib/shoperb_theme_editor/init.rb
@@ -1,20 +1,24 @@
-module Shoperb module Theme module Editor
-  class Init
+module Shoperb
+  module Theme
+    module Editor
+      class Init
 
-    Editor.autoload_all self, "init"
+        Editor.autoload_all self, "init"
 
-    def self.available_templates
-      Dir.glob("#{File.expand_path("../init/*", __FILE__)}").map(&Pathname.method(:new)).map(&:basename).map(&:to_s)
-    end
+        def self.available_templates
+          Dir.glob("#{File.expand_path("../init/*", __FILE__)}").map(&Pathname.method(:new)).map(&:basename).map(&:to_s)
+        end
 
-    def initialize template, handle
-      raise Error.new(
-        "No such template, possible options are #{self.class.available_templates.map(&:inspect).to_sentence}"
-      ) unless self.class.available_templates.include?(template)
-      Logger.notify "Copying #{template.inspect} template" do
-        FileUtils.cp_r("#{File.expand_path("../init/#{template}", __FILE__)}/.", Utils.base + ".")
+        def initialize template, handle
+          raise Error.new(
+            "No such template, possible options are #{self.class.available_templates.map(&:inspect).to_sentence}"
+          ) unless self.class.available_templates.include?(template)
+          Logger.notify "Copying #{template.inspect} template" do
+            FileUtils.cp_r("#{File.expand_path("../init/#{template}", __FILE__)}/.", Utils.base + ".")
+          end
+        end
+
       end
     end
-
   end
-end end end
+end
